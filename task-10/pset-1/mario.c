f9794218ba1264bc0d1a0e1d54a02f520301bcdd
JustinJiji <jijijohn002@gmail.com>
2021-11-19 23:07:08 +0530

    problem sets

---
+++ task-10/pset-1/mario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+    int n;
+    do{
+        printf("Height:");
+        scanf("%d",&n);
+    }while(n<1 || n>8);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i+j<n-1)
+            printf(" ");
+            else
+            printf("#");
+        }
+        printf("\n");
+    }
+}
